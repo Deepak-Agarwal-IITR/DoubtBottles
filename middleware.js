11fbd252283b99767c982b77b12fe64cd448e6ca
Deepak Agarwal <deepakjsagarwal@gmail.com>
2022-02-19 01:42:56 +0530

    Saving redirect URL before login

--- middleware.js
+++ middleware.js
@@ -4,6 +4,7 @@ const catchAsync = require("./utils/catchAsync");
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You musted be signed in')
         return res.redirect('/login')
     }
